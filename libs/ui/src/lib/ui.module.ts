cf0da8503223aaa389b3d2db16dbef31351d30be
David Brady <dgrbrady@gmail.com>
2021-04-18 13:22:39 -0400

    initial menu component commit

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from './loading/loading.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
-    LoadingComponent
+    LoadingComponent,
+    MenuComponent
   ],
   exports: [
-    LoadingComponent
+    LoadingComponent,
+    MenuComponent
   ],
 })
 export class UiModule {}
