785f842f6fde4842ba46b704d6318eedf8b41064
David Brady <dgrbrady@gmail.com>
2022-01-26 14:24:31 -0500

    adding sort-imports autofix eslint plugin

--- apps/desktop/src/app/app.module.ts
+++ apps/desktop/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { UiModule } from '@dgrbrady/ui';
 import { DataModule } from '@dgrbrady/data';
+import { UiModule } from '@dgrbrady/ui';
 
 @NgModule({
   declarations: [AppComponent],
