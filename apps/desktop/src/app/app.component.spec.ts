b98df4feae33b3ea6041b6526b1b2851ad9e02c4
David Brady <dgrbrady@gmail.com>
2022-02-21 11:22:44 -0500

    add cypress component tests for AppComponent

--- apps/desktop/src/app/app.component.spec.ts
+++ apps/desktop/src/app/app.component.spec.ts
@@ -1,36 +1,18 @@
 import { AppComponent } from './app.component';
-// import { TestBed } from '@angular/core/testing';
-import { initEnv, mount } from 'cypress-angular-unit-test';
-// import { TaskbarComponent, UiModule } from '@dgrbrady/ui';
-// import { Component } from '@angular/core';
-import { TestComponent } from './test.component';
+import { MockComponent } from 'ng-mocks';
 import { TaskbarComponent } from '@dgrbrady/ui';
-
+import { initEnv, mount } from 'cypress-angular-unit-test';
 
 describe('AppComponent', () => {
-  /* beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'desktop'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('desktop');
-  }); */
-
   it('should mount', () => {
-    initEnv(AppComponent);
+    initEnv(AppComponent, { declarations: [MockComponent(TaskbarComponent)] });
     const fixture = mount(AppComponent);
-    /* initEnv(TestComponent);
-    const fixture = mount(TestComponent); */
     expect(fixture).to.exist;
+  });
+
+  it('should contain TaskbarComponent', () => {
+    initEnv(AppComponent, { declarations: [MockComponent(TaskbarComponent)] });
+    mount(AppComponent);
+    cy.get('dgrbrady-taskbar');
   });
 });
--- apps/desktop/src/app/test.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'dgrbrady-test-component',
-  // template: '<div>test</div>'
-  templateUrl: './test.component.html'
-})
-export class TestComponent {}
-
